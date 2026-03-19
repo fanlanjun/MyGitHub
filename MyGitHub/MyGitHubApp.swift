6980a1257fcb2735d58bd9fd36aa564ea817a5e3
Frank Fan <lanjunfan@qq.com>
2026-03-19 22:10:50 +0800

    init project

---
+++ MyGitHub/MyGitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyGitHubApp.swift
+//  MyGitHub
+//
+//  Created by Frank Fan on 2026/3/18.
+//
+
+import SwiftUI
+
+@main
+struct MyGitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Podfile
@@ -0,0 +1,10 @@
+platform :ios, '14.0'
+use_frameworks!
+
+target 'MyGitHub' do
+  pod 'Alamofire', '~> 5.8'
+  pod 'Kingfisher', '~> 7.0'
+  pod 'SnapKit', '~> 5.6'
+  pod 'SwiftyJSON', '~> 5.0'
+  pod 'MBProgressHUD', '~> 1.2.0'
+end
